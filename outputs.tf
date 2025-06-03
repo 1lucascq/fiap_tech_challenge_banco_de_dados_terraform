f9bde47fac1cc93bbc0f71ffb842734ed22426e0
Lucas Quadros <1lucascq@gmail.com>
2025-06-03 20:34:37 -0300

    db setup with base ci/cd jobs

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,36 @@
 output "database_endpoint" {
   description = "Endpoint do banco PostgreSQL"
-  value       = aws_db_instance.fiap_postgres.endpoint
+  value       = aws_db_instance.postgres.endpoint
 }
 
 output "database_port" {
   description = "Porta do banco PostgreSQL"
-  value       = aws_db_instance.fiap_postgres.port
+  value       = aws_db_instance.postgres.port
 }
 
 output "database_name" {
   description = "Nome do banco de dados"
-  value       = aws_db_instance.fiap_postgres.db_name
+  value       = aws_db_instance.postgres.db_name
 }
 
 output "database_username" {
   description = "Username do banco"
-  value       = aws_db_instance.fiap_postgres.username
+  value       = aws_db_instance.postgres.username
   sensitive   = true
 }
 
 output "database_url" {
   description = "URL de conexão completa para usar na aplicação"
-  value       = "postgresql://${aws_db_instance.fiap_postgres.username}:${var.db_password}@${aws_db_instance.fiap_postgres.endpoint}:${aws_db_instance.fiap_postgres.port}/${aws_db_instance.fiap_postgres.db_name}?schema=public"
+  value       = "postgresql://${aws_db_instance.postgres.username}:${var.db_password}@${aws_db_instance.postgres.endpoint}:${aws_db_instance.postgres.port}/${aws_db_instance.postgres.db_name}?schema=public"
   sensitive   = true
 }
 
-output "vpc_id" {
-  description = "ID da VPC criada"
-  value       = aws_vpc.fiap_vpc.id
+output "rds_security_group_id" {
+  description = "ID do Security Group do RDS"
+  value       = aws_security_group.rds_sg.id
 }
 
-output "private_subnet_ids" {
-  description = "IDs das subnets privadas"
-  value       = [
-    aws_subnet.fiap_private_subnet_1.id,
-    aws_subnet.fiap_private_subnet_2.id
-  ]
+output "rds_subnet_group_name" {
+  description = "Nome do Subnet Group do RDS"
+  value       = aws_db_subnet_group.rds_subnet_group.name
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,7 @@
+variable "project_name" {
+  default = "tc-fiap"
+}
+
 variable "aws_region" {
   description = "Região da AWS"
   default     = "us-east-1"
@@ -15,6 +19,7 @@ variable "db_username" {
 
 variable "db_password" {
   description = "Senha do banco de dados"
+  default     = "senha_dificil"
   sensitive   = true
 }
 
@@ -26,11 +31,6 @@ variable "db_instance_class" {
 variable "db_allocated_storage" {
   description = "Espaço em disco (GB)"
   default     = 20
-}
-
-variable "project_name" {
-  description = "Nome do projeto"
-  default     = "fiap-tech-challenge"
 }
 
 variable "environment" {
